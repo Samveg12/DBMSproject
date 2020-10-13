5e4b27a1923b98628d8e98eddcad417d1e05fc74
Samveg12 <68071531+Samveg12@users.noreply.github.com>
2020-10-13 21:10:10 +0530

    Database in place

--- DBMS/settings.py
+++ DBMS/settings.py
@@ -132,5 +132,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'samvegvshah13@gmail.com'
-EMAIL_HOST_PASSWORD = "Samveg13@@"
+EMAIL_HOST_PASSWORD = "Samveg13@@@"
 print(EMAIL_HOST_PASSWORD)
